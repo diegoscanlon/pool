8af526033fce8e529da393c290e8edd0fb42b4b2
dscanlon <114500260+diegoscanlon@users.noreply.github.com>
2023-09-01 12:22:45 -0700

    minor fixes

--- js/account.js
+++ js/account.js
@@ -5,19 +5,32 @@ function redirectToPlanPage() {
 function showCancellationForm() {
     document.getElementById("cancellation-form").style.display = "block";
     document.getElementById("feedback-form").style.display = "none";
+    document.getElementById("new-match-form").style.display = "none";
 }
 
 function cancelTrip() {
-    const reason = document.getElementById("cancellation-reason").value;
-    alert("Your trip has been canceled. Reason: " + reason);
+    const cancel = document.getElementById("cancellation-reason").value;
+    alert("Your trip has been canceled. Reason: " + cancel);
 }
 
 function showFeedbackForm() {
     document.getElementById("feedback-form").style.display = "block";
     document.getElementById("cancellation-form").style.display = "none";
+    document.getElementById("new-match-form").style.display = "none";
 }
 
 function submitFeedback() {
     const feedback = document.getElementById("feedback-message").value;
     alert("Thank you for your feedback:\n" + feedback);
+}
+
+function showNewMatchForm() {
+    document.getElementById("feedback-form").style.display = "none";
+    document.getElementById("cancellation-form").style.display = "none";
+    document.getElementById("new-match-form").style.display = "block";
+}
+
+function newMatch() {
+    const newReason = document.getElementById("new-match-reason").value;
+    alert("We're finding you a new match! Sit tight!\n");
 }
